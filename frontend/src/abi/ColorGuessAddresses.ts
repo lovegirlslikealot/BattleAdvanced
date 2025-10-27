544f31015ca2b4571929e05e5a7d6b0b8823a8fb
lovegirlslikealot <lovegirlslikealot@proton.me>
2025-10-27 14:36:13 -0400

    Add ColorGuessAddresses.ts

---
+++ frontend/src/abi/ColorGuessAddresses.ts
@@ -0,0 +1,12 @@
+export const ColorGuessAddresses = {
+  "11155111": {
+    "chainId": 11155111,
+    "chainName": "sepolia",
+    "address": "0x34e69A4e8ecB41B99DcAD677E29fA281E8921849"
+  },
+  "sepolia": {
+    "chainId": 11155111,
+    "chainName": "sepolia",
+    "address": "0x34e69A4e8ecB41B99DcAD677E29fA281E8921849"
+  }
+} as const;
